8c0204924b6d8eff9a75566af469b837ce35cf03
jason40418 <jason40418@gmail.com>
2019-09-26 16:43:26 +0800

    Add: Member

---
+++ WEB-INF/classes/servletclass/Member.java
@@ -0,0 +1,28 @@
+package servletclass;
+
+public class Member {
+    private String email;
+    private String name;
+    private String password;
+
+    public Member() {
+    }
+
+    public Member(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
